56c9a415e4babbd0416cd645c3d4219f9b78fb8f
Nguyen-Phan-Minh-Tuan <88437031+Nguyen-Phan-Minh-Tuan@users.noreply.github.com>
2021-08-04 19:28:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
     introTitle: 'Ngọc à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Nghĩ đến em ... Nghĩ đến những lúc em cười với anh. Anh chỉ muốn dành cho em những lời ngọt ngào nhất, chân thành nhất .`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
